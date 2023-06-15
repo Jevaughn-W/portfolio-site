4a59e29e2350b35eb61e05f87a56eec879f62c91
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-06-15 18:08:34 -0400

    Add images to the project object

--- src/Components/Projects/projects.jsx
+++ src/Components/Projects/projects.jsx
@@ -8,19 +8,22 @@ const projects = [
     name: "HabTrack",
     description: "",
     technologies: "React, HTML, CSS, Javascript,PostgreSQL, Express, Nodejs, Axios, Material UI, Toast UI, ApexCharts & MomentJS",
-    github: "https://github.com/Jevaughn-W/HabitTracker"
+    github: "https://github.com/Jevaughn-W/HabitTracker",
+    image: "https://github.com/Jevaughn-W/HabitTracker/blob/master/docs/Screenshot%202023-03-22%20at%209.50.17%20PM.png?raw=true"
   },
   {
     name: "Lighthouse-labs Scheduler",
     description: "The app allows an user to create, edit and delete interviews based on 25 available slots, Monday through Friday. The app also includes features which notify users of errors such as a failed API requests or missing inputs when creating or changing an interview.",
     technologies: "React, HTML, Javascript, CSS, ostgreSQL, Express, Nodejs, Axios, Jest & Cypress",
-    github: "https://github.com/Jevaughn-W/lighthouse-labs-scheduler"
+    github: "https://github.com/Jevaughn-W/lighthouse-labs-scheduler",
+    image: "https://github.com/Jevaughn-W/lighthouse-labs-scheduler/blob/master/docs/Screen%20Shot%202023-01-16%20at%207.07.40%20AM.png?raw=true"
   },
   {
     name: "Food-Pick-Up-App",
     description: "This is a full stack web application designed to be a portal for users to pre-order food for pickup at a simulated restaurant called Dine&Dash.",
     technologies: "HTML5, CSS3, Javascript, Bootstrap, jQuery, Twillio API, SASS, PostgreSQL",
-    github: "https://github.com/Jevaughn-W/Food-Pick-Up-App"
+    github: "https://github.com/Jevaughn-W/Food-Pick-Up-App",
+    image: ""
   }
 ];
 
@@ -29,7 +32,7 @@ const projectList = projects.map((project, index)=> {
     <section key={index} className="project-card">
       <h4 className="title"><a href={project.github} target="_blank" rel="noopener noreferrer">{project.name}</a></h4>
       <div className="card-details">
-        <span>placeholder</span>
+        <img src={project.image} alt="HabTrack" height="50px" width="50px"/>
         <div className="project-description">
           <p><strong>Description:</strong> {project.description}</p>
           <p><strong>Tech Stack:</strong>{project.technologies}</p>
