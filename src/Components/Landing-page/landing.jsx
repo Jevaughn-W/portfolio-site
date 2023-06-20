a104ccb3abc45ec0ad4fbe0bc224f7ec7e6730f1
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-06-20 10:56:16 -0400

    Removed unnecessary Components

--- src/Components/Landing-page/landing.jsx
+++ src/Components/Landing-page/landing.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import NavBar from '../NavBar/NavBar';
 import './landing.css';
 
 
 export default function Landing() {
   return (
     <div id="landing-page">
+      <NavBar/>
       <h2>Jevaughn Williams</h2>
       <h3>Full-Stack Web Developer</h3>
       <h3>Scroll to explore</h3>
+
     </div>
   )
 }
