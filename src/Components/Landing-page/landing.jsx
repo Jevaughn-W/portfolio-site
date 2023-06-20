3570279660b5c508600b70fcbf1fbe3851de3a33
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-06-20 15:52:15 -0400

    Add arrow down animation

--- src/Components/Landing-page/landing.jsx
+++ src/Components/Landing-page/landing.jsx
@@ -11,9 +11,9 @@ export default function Landing() {
       <h3>Full-Stack Web Developer</h3>
       <h3>Scroll to explore</h3>
       <div className='down-arrow'>
-        <img src='images/down.png' width='50px' height='50px'/>
-        <img src='images/down.png' width='50px' height='50px'/>
-        <img src='images/down.png' width='50px' height='50px'/>
+        <img src='images/down.png' width='50px' height='50px' className='animated-image' key={1} alt='down'/>
+        <img src='images/down.png' width='50px' height='50px' className='animated-image' key={2} alt='down'/>
+        <img src='images/down.png' width='50px' height='50px' className='animated-image' key={3} alt='down'/>
       </div>
 
     </div>
