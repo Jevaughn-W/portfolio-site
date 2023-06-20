a0b481f676a3d33894b1084583f47680567624d2
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-06-20 08:38:14 -0400

    Add landing page component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import About from './Components/About/About';
 import Projects from './Components/Projects/Projects';
+import Landing from './Components/Landing-page/landing';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Landing/>
       <About/>
       <Projects/>
     </div>
