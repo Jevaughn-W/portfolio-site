e07c5f09a3b9be6002fc551b6e6c99eb70867b10
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-06-20 11:23:28 -0400

    Remove navbar from flow

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import NavBar from './Components/NavBar/NavBar';
 import About from './Components/About/About';
 import Projects from './Components/Projects/Projects';
 import Landing from './Components/Landing-page/landing';
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Landing/>
-      <NavBar/>
       <About/>
       <Projects/>
     </div>
