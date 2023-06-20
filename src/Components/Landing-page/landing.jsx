01deb0c5f7707cb1a89b7be3e024f999d6eeea2d
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-06-20 10:17:11 -0400

    Add content and format landing page

--- src/Components/Landing-page/landing.jsx
+++ src/Components/Landing-page/landing.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './landing.css';
 
 
 export default function Landing() {
   return (
-    <div>
-      hello
+    <div id="landing-page">
+      <h2>Jevaughn Williams</h2>
+      <h3>Full-Stack Web Developer</h3>
+      <h3>Scroll to explore</h3>
     </div>
   )
 }
