c5917933e8ceb1df365e5e6af637269daa772734
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-06-22 08:33:37 -0400

    Clean up paragraph section and update view height of the component

--- src/Components/About/about.jsx
+++ src/Components/About/about.jsx
@@ -3,10 +3,7 @@ import './about.css';
 
 export default function About() {
   
-  const aboutSummary = `I am a Full-Stack Web Developer recently graduated from the Lighthouse Labs Web Development bootcamp. I enjoy building web apps that solves real world problems and crave opportunities to continue growing as a web developer.
-
-  Prior to diving into the world of web development, I completed a Bsc. in Actuarial Science and worked as a consultant in PwC’s Value Creation practice. During my years as a consultant I have worked on numerous transactions where I have honed my ability to work on high pressure, time sensitive projects as well as developed strong communication skills.`;
-
+ 
   return(
     <div id="about">
       <div className='header'>
@@ -15,7 +12,7 @@ export default function About() {
       <article className='summary'>
         <div className='about-container'>
           <img src="images/portfolio-img-placeholder.jpeg"/>
-          <p>{aboutSummary}</p>
+          <p>I am a Full-Stack Web Developer recently graduated from the Lighthouse Labs Web Development bootcamp. I enjoy building web apps that solves real world problems and crave opportunities to continue growing as a web developer. <br/> <br/>Prior to diving into the world of web development, I completed a Bsc. in Actuarial Science and worked as a consultant in PwC’s Value Creation practice. During my years as a consultant I have worked on numerous transactions where I have honed my ability to work on high pressure, time sensitive projects as well as developed strong communication skills.</p>
         </div>
       </article>
     </div>
