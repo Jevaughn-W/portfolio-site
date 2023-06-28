82373816e553c0c6cb7dafd6ad5a9b03dffce323
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-06-28 08:16:32 -0400

    Remove navbar from landing page component

--- src/Components/Landing-page/landing.jsx
+++ src/Components/Landing-page/landing.jsx
@@ -6,7 +6,6 @@ import './landing.css';
 export default function Landing() {
   return (
     <div id="landing-page">
-      <NavBar/>
       <h2>Jevaughn Williams</h2>
       <h3>Full-Stack Web Developer</h3>
       <h3>Scroll to explore</h3>
