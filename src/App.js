28c251f31dc91b7433d11b9bf3aa9eeb2561fec4
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-06-28 08:21:20 -0400

    Add navbar to the app component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import About from './Components/About/about';
 import Projects from './Components/Projects/projects';
 import Landing from './Components/Landing-page/landing';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Landing/>
       <About/>
       <Projects/>
