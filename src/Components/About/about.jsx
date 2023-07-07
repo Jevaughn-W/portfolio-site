aa0a0c1f45a961a28e2aeb55acc31abc1417c5fb
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-07-07 00:02:47 -0400

    Add alt to profile image

--- src/Components/About/about.jsx
+++ src/Components/About/about.jsx
@@ -11,7 +11,7 @@ export default function About() {
       </div>
       <article className='summary'>
         <div className='about-container'>
-          <img src="images/portfolio-img-placeholder.jpeg"/>
+          <img src="images/portfolio-img-placeholder.jpeg" alt="Jevaughn Williams"/>
           <p>I am a Full-Stack Web Developer recently graduated from the Lighthouse Labs Web Development bootcamp. I enjoy building web apps that solves real world problems and crave opportunities to continue growing as a web developer. <br/> <br/>Prior to diving into the world of web development, I completed a Bsc. in Actuarial Science and worked as a consultant in PwC’s Value Creation practice. During my years as a consultant I have worked on numerous transactions where I have honed my ability to work on high pressure, time sensitive projects as well as developed strong communication skills.</p>
         </div>
       </article>
